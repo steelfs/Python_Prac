f79f7f7ff3dc6cf49e4a7a535e8fff1e45c200c9
steelfs <steelfs7@gmail.com>
2023-11-23 21:39:23 +0900

    Create DataType.py

---
+++ DataType.py
@@ -0,0 +1,19 @@
+
+import random
+# class fruit:
+#     pass
+# a = fruit
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+a = float(x)
+b = int(y)
+print(y) # result = 2.8 
+print(b) # result  = 2
+
+c = complex(x)
+print(c)# result = (1+0j)
+
+
+print(random.randrange(1, 10))
