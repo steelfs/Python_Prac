0429c3450ec9ca36628873602402813604a5d8eb
steelfs <steelfs7@gmail.com>
2023-11-24 07:37:43 +0900

    update string/py

---
+++ String.py
@@ -0,0 +1,22 @@
+
+#slicing
+# word = 'banana'
+# for i in word[2:]:
+#     print(i)
+b = 'Hello, world!'
+# print(b[2:5])  [2:5] 2 would be start index(inclusive), 5 is end index(exclusive) 
+#print(b[:2]) # if not define start index. default is 0 that means 0 to 2 = 'He'
+#print(b[5:])# if not define end index. default is last index.
+print(b[-5:-2])
+
+#get Length
+# word = "Hello world"
+# print(len(word))
+
+#checking exist word
+# txt = "The best things in life are fre!"
+# print("expensive" not in txt)
+
+# if "expensive" not in txt:
+#     print("there is no 'expensive'")
+
