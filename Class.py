e0e4dbd5048d2e9ee0d440342e629a5731d3bcf7
steelfs <steelfs7@gmail.com>
2023-11-27 07:28:13 +0900

    Update Class.py

--- Class.py
+++ Class.py
@@ -1,27 +1,49 @@
-class MyClass:
-    def __init__(self, name : str, hp : int) -> None:
+class person:
+    def __init__(self, name : str, speed : int) -> None:
         self.name = name
-        self.hp = hp
+        self.speed  = speed
         
-    @staticmethod
-    def PrintName(self):
-        print(f'name : {self.name} is created.')
+class child(person):
+    def __init__(self, name: str, speed: int) -> None:
+        super().__init__(name, speed)
         
-    def __str__(self) -> str:
-        return f"Name : {self.name}"    
-    
-class MyClass2:
-    def __init__(self, name : str, hp : int) -> None:
-        self.name = name
-        self.hp = hp
-    
-mc1 = MyClass('mc1', 100)
-mc2 = MyClass2('mc2', 200)
+a = child('J', 100)
 
-mc1.hp = mc2.hp
+print(a.name)
 
-print(id(mc1.hp))
-print(id(mc2.hp))
-del mc2
-print(mc1.hp)
+
+
+
+
+
+
+
+
+
+# class MyClass:
+#     def __init__(self, name : str, hp : int) -> None:
+#         self.name = name
+#         self.hp = hp
+        
+#     @staticmethod
+#     def PrintName(self):
+#         print(f'name : {self.name} is created.')
+        
+#     def __str__(self) -> str:
+#         return f"Name : {self.name}"    
+    
+# class MyClass2:
+#     def __init__(self, name : str, hp : int) -> None:
+#         self.name = name
+#         self.hp = hp
+    
+# mc1 = MyClass('mc1', 100)
+# mc2 = MyClass2('mc2', 200)
+
+# mc1.hp = mc2.hp
+
+# print(id(mc1.hp))
+# print(id(mc2.hp))
+# del mc2
+# print(mc1.hp)
 
